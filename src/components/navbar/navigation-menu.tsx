4a5dfde04a6e03e948f7474b9a90ab1e9f7b29a4
lineonthepaper <77160167+lineonthepaper@users.noreply.github.com>
2024-10-08 11:42:26 +0800

    remove unused

--- src/components/navbar/navigation-menu.tsx
+++ src/components/navbar/navigation-menu.tsx
@@ -1,12 +1,7 @@
 import {
     NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
     NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
     navigationMenuTriggerStyle
   } from "@/components/ui/navigation-menu";
 
