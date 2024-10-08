2ed8b8694f9e60ed12a87af83e62d7b78663bb01
lineonthepaper <77160167+lineonthepaper@users.noreply.github.com>
2024-10-08 16:01:02 +0800

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,47 @@
 import { CardHeader, Card, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import {Separator} from "@/components/ui/separator";
 import { Checkbox } from "@/components/ui/checkbox";
-
-
+import Drugs from "@/app/assets/drugs.jpeg";
+import Rat_Meds from "@/app/assets/rat_meds.jpg"
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid justify-center">
-      <Card className="w-[1000px] grid justify-center">
-        <CardHeader>
-          <CardTitle>Drug abuse</CardTitle>
-          <CardDescription>is when you abuse drugs</CardDescription>
-        </CardHeader>
-        <Separator className="my-4"/>
-        <CardContent>
-          <p>{"don't abuse drugs kids"}</p>
-          <div className="flex space-x-2">
-
-
-
+    <div>
+    <div className="flex flex-col justify-center min-h-dvh text-center items-center gap-y-5">
+        <div className="grid gap-y-1 w-1/3 min-w-52">
+          <p className="font-bold text-3xl">Drug abuse</p>
+          <p className="text-sm opacity-70 italic">is when you abuse drugs</p>
+          <p className="text-xs opacity-40 italic">{"more specifically, it's when you use drugs for a non-medical reason, in amounts that are harmful to you, and you do it a lot"}</p>
+        </div>
+        <Separator className="w-1/2" />
+        <div>
+          <p className="font-bold text-xl">{"don't abuse drugs kids"}</p>
+          <br></br>
+          <div className="flex items-top space-x-2">
           <Checkbox id="dontabusedrugs"/>
-          <label htmlFor="dontabusedrugs" className="text-sm font-medium leading-none">ok?</label>
+          <div className="grid gap-1.5 leading-none">
+          <label htmlFor="dontabusedrugs" className="text-sm font-medium leading-none place-items-end">ok?</label>
           </div>
-        </CardContent>
-      </Card>
+          </div>
+          <br></br>
+        </div>
+        <Card className="grid place-items-center">
+          <CardHeader className="gap-y-3">
+            <CardTitle>Drugs</CardTitle>
+            <CardDescription className="grid place-items-center">
+              <Image src={Drugs} height={200} alt="Drugs"/>
+            </CardDescription>
+            <Separator/>
+            <CardTitle>Use drugs responsibly, like him</CardTitle>
+            
+            <CardDescription className="grid place-items-center">
+              <Image src={Rat_Meds} height={300} alt="Rat using drugs responsibly"/>
+            </CardDescription>
+          </CardHeader>
+        </Card>
+          
+      </div>
     </div>
 
   );
--- src/components/navbar/navigation-menu.tsx
+++ src/components/navbar/navigation-menu.tsx
@@ -10,29 +10,34 @@ import { ModeToggle } from "../mode-toggle";
 
 export function NavigationBar() {
     return(
-    <NavigationMenu className="list-none h-16">
-        <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-                </NavigationMenuLink>
-            </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-            <Link href="/quiz" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Quiz
-                </NavigationMenuLink>
-            </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-            <Link href="/about-me" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About me
-                </NavigationMenuLink>
-            </Link>
-        </NavigationMenuItem>
-        <ModeToggle></ModeToggle>
+    <NavigationMenu className="flex flex-row justify-between min-w-full list-none h-16 sticky top-0">
+        <div className="flex flex-row">
+            <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/quiz" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Quiz
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/about-me" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About me
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+        </div>
+        
+        <div>
+        <ModeToggle/>
+        </div>
     </NavigationMenu>
     )
 }
